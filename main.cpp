10058a11d7beac72845d20b1268fd94cb4a26ac4
aderly2 <aderly@gmail.com>
2015-05-09 17:02:06 -0600

    dawdawd

--- main.cpp
+++ main.cpp
@@ -22,18 +22,57 @@ int generateRandom(int max)
 
 int main(int, char const**)
 {
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/cute_image.jpg")) {
+        return EXIT_FAILURE;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // Player Object
     player player;
+    player.sprite.setTexture(texture);
     
     // Enemy Vector Array (Stores Enemy Objects)
     class enemy enemyArray;
     vector<enemy>::const_iterator iter;
     std::vector<enemy> theEnemyArray;
     
+    // Enemy Object
+    enemy enemy;
+    enemy.sprite.setTexture(texture);
+    
     // Object Vector Array (Stores Objects On The Ground)
     class object objectArray(0);
     vector<object>::const_iterator iter2;
     std::vector<object> theObjectArray;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Rogue-Like RPG 1.00");
@@ -45,13 +84,6 @@ int main(int, char const**)
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("resources/cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
     // Create a graphical text to display
     sf::Font font;
     if (!font.loadFromFile("resources/sansation.ttf")) {
@@ -59,15 +91,6 @@ int main(int, char const**)
     }
     sf::Text text("Hello SFML", font, 50);
     text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("resources/nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    //music.play();
 
     // Start the game loop
     while (window.isOpen())
@@ -90,66 +113,107 @@ int main(int, char const**)
         // Clear screen
         window.clear();
         
+        // Player Movement ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // Player Vector Movement
+            float tempXSize = window.getSize().x;
+            float tempYSize = window.getSize().y;
+            sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+            sf::Vector2f worldPos = window.mapPixelToCoords(localPosition);
+            float mx = (worldPos.x)/tempXSize;
+            float my = (worldPos.y)/tempYSize;
+            float x = (player.rect.getPosition().x)/tempXSize;
+            float y = (player.rect.getPosition().y)/tempYSize;
+            float difx = mx - x;
+            float dify = my - y;
+            dify = dify * -1;
+            float angle = atan2(difx,dify) * (180/3.1415);
+            player.rect.setRotation(angle);
+            
+            if (playerCanMove == true)
+            {
+                // PLAYER MOVEMENT (WASD)
+                float movementSpeed = player.movementSpeed;
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                {
+                    player.rect.move(0,-movementSpeed);
+                    player.playerAnimationState = 1; // Move Up
+                    player.walkingAnimationLengthCounter = 0;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    player.rect.move(-movementSpeed,0);
+                    player.playerAnimationState = 1; // Move Left
+                    player.walkingAnimationLengthCounter = 0;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    player.rect.move(0,movementSpeed);
+                    player.playerAnimationState = 1; // Move Down
+                    player.walkingAnimationLengthCounter = 0;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    player.rect.move(movementSpeed,0);
+                    player.playerAnimationState = 1; // Move Right
+                    player.walkingAnimationLengthCounter = 0;
+                }
+            }
         
-        // Player Vector Movement
-        float tempXSize = window.getSize().x;
-        float tempYSize = window.getSize().y;
-        sf::Vector2i localPosition = sf::Mouse::getPosition(window);
-        sf::Vector2f worldPos = window.mapPixelToCoords(localPosition);
-        float mx = (worldPos.x)/tempXSize;
-        float my = (worldPos.y)/tempYSize;
-        float x = (player.rect.getPosition().x)/tempXSize;
-        float y = (player.rect.getPosition().y)/tempYSize;
-        float difx = mx - x;
-        float dify = my - y;
-        dify = dify * -1;
-        float angle = atan2(difx,dify) * (180/3.1415);
-        player.rect.setRotation(angle);
         
-        if (playerCanMove == true)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // DRAW ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        
+        // Draw Objects
+        counter = 0;
+        for (iter2 = theObjectArray.begin(); iter2 != theObjectArray.end(); iter2++)
         {
-            // PLAYER MOVEMENT (WASD)
-            float movementSpeed = player.movementSpeed;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            {
-                player.rect.move(0,-movementSpeed);
-                player.playerAnimationState = 1; // Move Up
-                player.walkingAnimationLengthCounter = 0;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            {
-                player.rect.move(-movementSpeed,0);
-                player.playerAnimationState = 1; // Move Left
-                player.walkingAnimationLengthCounter = 0;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            {
-                player.rect.move(0,movementSpeed);
-                player.playerAnimationState = 1; // Move Down
-                player.walkingAnimationLengthCounter = 0;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-                player.rect.move(movementSpeed,0);
-                player.playerAnimationState = 1; // Move Right
-                player.walkingAnimationLengthCounter = 0;
-            }
+            theObjectArray[counter].update(); // Update Items
+            //window.draw(theObjectArray[counter].rect);
+            window.draw(theObjectArray[counter].sprite);
+
+            counter++;
         }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        // Draw Enemies
+        counter = 0;
+        for (iter = theEnemyArray.begin(); iter != theEnemyArray.end(); iter++)
+        {
+            //window.draw(theEnemyArray[counter].rect);
+            window.draw(theEnemyArray[counter].sprite);
+            //window.draw(theEnemyArray[counter].text);
+            counter++;
+        }
         
         // Draw Player
         window.draw(player);
