ceec1bde1108980530c1f6d5db54269106489f51
aderly2 <aderly@gmail.com>
2015-05-09 16:20:42 -0600

    dawdawd

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-//
-// Disclamer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resource, use the helper
-// method resourcePath() from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <random>
+#include <cmath>
+#include <vector>
+#include <unistd.h>
 #include <iostream>
 using namespace std;
 
